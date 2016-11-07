3c44b9be3fb7e80287a1aa82c934263761b9fb28
Stanislav Prokop <prokopst.git@outlook.com>
2016-11-07 23:28:17 +0100

    #3 removed yagni parameter.

--- acetone/resolver.py
+++ acetone/resolver.py
@@ -1,15 +1,14 @@
 import importlib
 
 
-def resolve_type(type_name: str, class_separator: str='.'):
+def resolve_type(type_name: str):
     """
-    Resolves type in format 'module.submodule.ClassName' where the class separator can be set
-    via class_separator.
+    Resolves type in format 'module.submodule.ClassName'.
 
     :param type_name: name in format 'module.submodule.ClassName'
     :rtype: type
     """
-    module_name, _, class_name = type_name.rpartition(class_separator)
+    module_name, _, class_name = type_name.rpartition('.')
 
     module = importlib.import_module(module_name)
     factory = getattr(module, class_name)
